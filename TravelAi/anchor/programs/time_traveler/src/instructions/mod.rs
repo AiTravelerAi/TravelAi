42010483b2cd45efe460c0e594767e3cb1c9ee64
TravelAi <dogedogedogedoge000@gmail.com>
2025-09-12 16:12:36 +0200

    Update mod.rs

--- TravelAi/anchor/programs/time_traveler/src/instructions/mod.rs
+++ TravelAi/anchor/programs/time_traveler/src/instructions/mod.rs
@@ -1,1 +1,14 @@
+pub mod init_registry;
+pub mod set_authority;
+pub mod set_config;
+pub mod create_pool;
+pub mod contribute;
+pub mod verify_and_close_pool;
+
+pub use init_registry::*;
+pub use set_authority::*;
+pub use set_config::*;
+pub use create_pool::*;
+pub use contribute::*;
+pub use verify_and_close_pool::*;
 // mod.rs for instructions
